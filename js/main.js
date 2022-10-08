2cb06a9541cc2f79337ba46bac4309f252030a07
Raul <110264497+Raulpss@users.noreply.github.com>
2022-10-08 18:38:00 -0300

    js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ var button = document.querySelector(".create-account button");
 button.onclick = function() {
     window.location.href = "https://tela-cadastro-j839.vercel.app/";
 }
+
+//ABRIR PAGINA DE REDEFINIR SENHA
+var button = document.querySelector(".forget-password button");
+
+button.onclick = function() {
+    window.location.href = "https://tela-redefinir-senha.vercel.app/";
+}
