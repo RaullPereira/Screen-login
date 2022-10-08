b76d03f09566edde5af3ebc4004556ad9544e52e
Raul <110264497+Raulpss@users.noreply.github.com>
2022-10-08 18:35:06 -0300

    js implementado

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//ABRIR PAGINA DE LOGIN
+var button = document.querySelector(".create-account button");
+
+button.onclick = function() {
+    window.location.href = "https://tela-cadastro-j839.vercel.app/";
+}
